db2f2263b78823d786b4879af9279e79d0ff8000
Jillian Arber <snowypig@jumping-jack-138.dynamic2.rpi.edu>
2015-12-03 15:27:20 -0500

    tree-growing code

---
+++ Assets/Factory.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Factory : MonoBehaviour {
+	GameObject fact1;
+	GameObject fact2;
+	public GUIText info;
+	// Use this for initialization
+	void Start () {
+		fact1 = GameObject.Find ("Cube");
+		fact2 = GameObject.Find ("Cube1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
