95a9349a827ae7bc71ac55bfd9692708ea15264f
Philip Cioni <philcioni@gmail.com>
2015-12-08 16:52:28 -0500

    merged my camera controller with Will's

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -6,24 +6,24 @@ public class cameraController : MonoBehaviour {
 	public float sensitivity  = 0.0f;
 	public float speed = 0.0f;
 	public float distanceFromZoomTarget = 0;  // how far from orbitTarget lerp stops
-
+	
 	private float minFov = 60.0f;
 	private float maxFov = 120.0f;
-
+	
 	private GameObject orbitTarget;
 	private GameObject originPlanet;
-
+	
 	private Vector3 lookPos;
 	private Quaternion rotation;
 	private Quaternion lookAtAngle;           // target slerp angle
 	private Vector3 relativePos;              // relative camera position from OrbitTarget
-
+	
 	private float startTime;                  // time our lerp begins
 	private float journeyLength;			  // distance between both lerp objects
 	private Transform startMarker;            
 	private Transform endMarker;
 	private Vector3 lerpVector;               // point on a line to lerp to.
-
+	
 	private bool canRotateCamera; // don't allow rotation during Lerp / Slerp.
 	
 	Vector3 camSmoothDampV; 
@@ -54,34 +54,29 @@ public class cameraController : MonoBehaviour {
 				orbitTarget.GetComponent<objectHighlightOnMouseover>().deselect();
 				orbitTarget = hitObject;
 				orbitTarget.GetComponent<objectHighlightOnMouseover>().select();
-
-				//Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, hitObject.transform.position, );
-				//Camera.main.transform.position = hitObject.transform.position;
-				Camera.main.transform.rotation = hitObject.transform.rotation;
-				Camera.main.transform.Translate(2, 0, 0);
-
+				
 				lookPos = orbitTarget.transform.position - Camera.main.transform.position;
 				rotation = Quaternion.LookRotation(lookPos);
-
+				
 				//keeps track of our lerp distance
 				endMarker = orbitTarget.transform;
 				startMarker = transform;
 				startTime = Time.time;
 				journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
-
+				
 				//slerp smoothly
 				relativePos = orbitTarget.transform.position - transform.position;
 				lookAtAngle = Quaternion.LookRotation(relativePos);
-
+				
 				//Get a normalized vector projected towards orbitTarget
 				lerpVector = endMarker.position - startMarker.position;
 				lerpVector = lerpVector.normalized;
-
+				
 				StartCoroutine("smoothDampToPlanet");
 			}
 		}
 	}    
-
+	
 	// H-A-T-E, this is what stress does to me!
 	float clampAngle(float angle, float min, float max) {
 		
@@ -94,12 +89,12 @@ public class cameraController : MonoBehaviour {
 		if (angle < 0) angle += 360;        // if angle negative, convert to 0..360
 		return angle;
 	}
-
+	
 	//Orbit around orbitTarget's X-axis. Allows full 360 degree movement.
 	void orbitCamera() {
 		if (Input.GetMouseButton(1) && canRotateCamera) {
 			transform.RotateAround( orbitTarget.transform.position, orbitTarget.transform.up, Input.GetAxis("Mouse X") * speed);
-
+			
 			Vector3 cross = Vector3.Cross(transform.forward, transform.up);
 			transform.RotateAround( orbitTarget.transform.position, cross, Input.GetAxis("Mouse Y") * speed);
 		}
@@ -109,15 +104,15 @@ public class cameraController : MonoBehaviour {
 	void resetCameraToOrigin() {
 		orbitTarget.GetComponent<objectHighlightOnMouseover>().deselect();
 		orbitTarget = originPlanet;
-
+		
 		endMarker = orbitTarget.transform;
 		startMarker = transform;
 		startTime = Time.time;
 		journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
-
+		
 		StartCoroutine("smoothDampToPlanet");
 	}
-
+	
 	/*
 	 * Slerp from the camera's current rotation to lookAtAngle, a Quaternion
 	 *   made in zoomInOnTarget.
@@ -133,8 +128,8 @@ public class cameraController : MonoBehaviour {
 			float distCovered = (Time.time - startTime) * speed;
 			float fracJourney = distCovered / journeyLength;
 			Vector3 lerpTo = (endMarker.position + (distanceFromZoomTarget * lerpVector));
-			Vector3 matchHeight = new Vector3(lerpTo.x, orbitTarget.transform.position.y, lerpTo.z);
-			lerpTo = matchHeight;
+			//Vector3 matchHeight = new Vector3(lerpTo.x, orbitTarget.transform.position.y, lerpTo.z);
+			//lerpTo = matchHeight;
 			
 			transform.position = Vector3.Lerp(startMarker.position, lerpTo, fracJourney);
 			transform.rotation = Quaternion.Slerp(startMarker.rotation, lookAtAngle, Time.deltaTime * speed); // 2 = damping
@@ -160,13 +155,12 @@ public class cameraController : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Escape)) {
 			resetCameraToOrigin();
 		}
-
-
+		
+		
 		orbitCamera ();
-	
+		
 		
 	}
 	
 	
 }
-		
