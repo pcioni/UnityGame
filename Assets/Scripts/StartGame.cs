67b450593385b3b41b4045cf4e22952506ed91ca
Jillian Arber <snowypig@jumping-jack-35.dynamic2.rpi.edu>
2015-12-09 01:08:28 -0500

    Win and fail screens start screen + game-starting code

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	public Button init_game;
+	public Image init_screen;
+
+	void Start () {
+		Time.timeScale = 0.0f;
+		init_screen.gameObject.SetActive (true);
+		init_game.gameObject.SetActive (true);
+		init_game.onClick.AddListener (delegate () {
+			this.ButtonClicked ();
+		});
+	}
+	
+
+
+	void Update () {
+	
+	}
+
+	public void ButtonClicked(){
+		Time.timeScale = 1.0f;
+		init_screen.gameObject.SetActive (false);
+		init_game.gameObject.SetActive (false);
+	}
+}
